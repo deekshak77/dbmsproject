4033fc1e4988c195471e267820b4ae97d02a0c6f
AneeshMBhat <aneeshbhat3719@gmail.com>
2023-01-03 19:58:55 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,7 @@ function App() {
   return (
     <>
       <NavBar setActiveBody={setActiveBody} />
+      {getBody()}
     </>
   );
 }
--- frontend/src/Houses.js
+++ frontend/src/Houses.js
@@ -1,3 +1,48 @@
+import { Col, Container, Row, Table } from "react-bootstrap";
+
 export default function Houses() {
-  return <div></div>;
+  return (
+    <Container fluid>
+      <Row className="g-0">
+        <Col>
+          <Card border="primary" style={{ width: "19vw", height: "90vh" }}>
+            {/* <CRUDCardBody
+              setActiveComponentLeftCard={(str) =>
+                setActiveComponentLeftCard(str)
+              }
+            /> */}
+          </Card>
+        </Col>
+        <Col>
+          <Card border="primary" style={{ width: "78vw", height: "90vh" }}>
+            <Card.Body>
+              <Card.Title>Table View</Card.Title>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Book Id</th>
+                    <th>Book Name</th>
+                    <th>Published Year</th>
+                    {/* {activeAttribute()} */}
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* {books.map((book, index) => (
+                    <tr key={book.id}>
+                      <td>{index + 1}</td>
+                      <td>{book.bookId}</td>
+                      <td>{book.bookName}</td>
+                      <td>{book.publishedYear}</td>
+                      {activeButton(book.id)}
+                    </tr>
+                  ))} */}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
