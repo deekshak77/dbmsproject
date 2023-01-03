8ab8f15de718f83834f56ffd65f3b744c865bc04
AneeshMBhat <aneeshbhat3719@gmail.com>
2023-01-04 00:58:12 +0530

    done

--- frontend/src/HousesWithOwners.js
+++ frontend/src/HousesWithOwners.js
@@ -1,48 +1,123 @@
 import Layout from "./Layout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
+import axios from "axios";
 
 export default function HousesWithOwners() {
   const [isFormActive, setIsFormActive] = useState(false);
+
+  const [customerId, setCustomerId] = useState("");
+  const [houseId, setHouseId] = useState("");
+  const [rentStartDate, setRentStartDate] = useState("");
+  const [rentDuration, setRentDuration] = useState("");
+  const [monthlyRent, setMonthlyRent] = useState("");
+  const [id, setId] = useState("");
+
   const addHandler = () => {
+    setCurrentAction(0);
     setIsFormActive(true);
   };
-  const updateHandler = () => {};
-  const deleteHandler = () => {};
+  const updateHandler = () => {
+    setCurrentAction(1);
+  };
+  const deleteHandler = () => {
+    setCurrentAction(2);
+  };
+
+  useEffect(() => {
+    getRentedHouses();
+  }, []);
+
+  const [rentedHouses, setRentedHouses] = useState([]);
+  const [currentAction, setCurrentAction] = useState(0);
+
+  const getRentedHouses = async () => {
+    const response = await axios.get("http://localhost:5000/rented");
+    setRentedHouses(response.data);
+  };
+  const updateRentedHouse = async () => {
+    await axios.patch(`http://localhost:5000/rented/${id}`, {
+      customerId: customerId,
+      houseId: houseId,
+      rentStartDate: rentStartDate,
+      rentDuration: rentDuration,
+      monthlyRent: monthlyRent,
+    });
+    getRentedHouses();
+  };
+  const deleteRentedHouse = async (ide) => {
+    await axios.delete(`http://localhost:5000/rented/${ide}`);
+    getRentedHouses();
+  };
+  const createRentedHouse = async () => {
+    await axios.post("http://localhost:5000/rented", {
+      customerId: customerId,
+      houseId: houseId,
+      rentStartDate: rentStartDate,
+      rentDuration: rentDuration,
+      monthlyRent: monthlyRent,
+    });
+    getRentedHouses();
+  };
+  const getRentedHouseById = async (ide) => {
+    const response = await axios.get(`http://localhost:5000/rented/${ide}`);
+    setCustomerId(response.data.customerId);
+    setRentStartDate(response.data.rentStartDate);
+    setHouseId(response.data.houseId);
+    setRentDuration(response.data.rentDuration);
+    setMonthlyRent(response.data.monthlyRent);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    switch (currentAction) {
+      case 0:
+        createRentedHouse();
+        break;
+      case 1:
+        updateRentedHouse();
+        break;
+    }
+    setIsFormActive(false);
+  };
+  const updateTableHandler = (ide) => {
+    setId(ide);
+    getRentedHouseById(ide);
+    setIsFormActive(true);
+  };
+  const deleteTableHandler = (ide) => {
+    setId(ide);
+    deleteRentedHouse(ide);
+  };
+
   const leftForm = () => {
     return (
       <Card.Body>
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setIsFormActive(false);
-          }}
-        >
-          <Form.Group className="mb-2" controlId="FormBookId">
-            <Form.Label>Book Id</Form.Label>
+        <Form onSubmit={(e) => submitHandler(e)}>
+          <Form.Group classHouseId="mb-2" controlId="FormBookId">
+            <Form.Label>Customer HouseId</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Book Id"
-              //   value={bookId}
-              //   onChange={(event) => setBookId(event.target.value)}
+              placeholder="Enter Customer HouseId"
+              value={houseId}
+              onChange={(event) => setHouseId(event.target.value)}
             />
           </Form.Group>
-          <Form.Group className="mb-2" controlId="FormBookName">
-            <Form.Label>Book Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Book Name"
-              //   value={bookName}
-              //   onChange={(event) => setBookName(event.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-2" controlId="FormPublishedYear">
-            <Form.Label>Published Year</Form.Label>
+          <Form.Group classHouseId="mb-2" controlId="FormBookHouseId">
+            <Form.Label>Phone Number</Form.Label>
             <Form.Control
               type="number"
-              placeholder="Published Year"
-              //   value={publishedYear}
-              //   onChange={(event) => setPublishedYear(event.target.value)}
+              placeholder="Enter Phone Number"
+              value={customerId}
+              onChange={(event) => setCustomerId(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group classHouseId="mb-2" controlId="FormPublishedYear">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Email Address"
+              value={rentStartDate}
+              onChange={(event) => setRentStartDate(event.target.value)}
             />
           </Form.Group>
           <Button type="submit" style={{ width: "100%", marginTop: "2vh" }}>
@@ -58,9 +133,9 @@ export default function HousesWithOwners() {
       addHandler={addHandler}
       updateHandler={updateHandler}
       deleteHandler={deleteHandler}
-      data={[{}]}
+      data={rentedHouses}
       headers={["Owner Id", "House Id", "Current Occupancy Status"]}
-      keys={[]}
+      keys={["ownerId", "houseId", ""]}
       leftForm={leftForm}
     />
   );
--- frontend/src/RentedHouses.js
+++ frontend/src/RentedHouses.js
@@ -1,48 +1,141 @@
 import Layout from "./Layout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
+import axios from "axios";
 
 export default function RentedHouses() {
   const [isFormActive, setIsFormActive] = useState(false);
+
+  const [customerId, setCustomerId] = useState("");
+  const [houseId, setHouseId] = useState("");
+  const [rentStartDate, setRentStartDate] = useState("");
+  const [rentDuration, setRentDuration] = useState("");
+  const [monthlyRent, setMonthlyRent] = useState("");
+  const [id, setId] = useState("");
+
   const addHandler = () => {
+    setCurrentAction(0);
     setIsFormActive(true);
   };
-  const updateHandler = () => {};
-  const deleteHandler = () => {};
+  const updateHandler = () => {
+    setCurrentAction(1);
+  };
+  const deleteHandler = () => {
+    setCurrentAction(2);
+  };
+
+  useEffect(() => {
+    getRentedHouses();
+  }, []);
+
+  const [rentedHouses, setRentedHouses] = useState([]);
+  const [currentAction, setCurrentAction] = useState(0);
+
+  const getRentedHouses = async () => {
+    const response = await axios.get("http://localhost:5000/rented");
+    setRentedHouses(response.data);
+  };
+  const updateRentedHouse = async () => {
+    await axios.patch(`http://localhost:5000/rented/${id}`, {
+      customerId: customerId,
+      houseId: houseId,
+      rentStartDate: rentStartDate,
+      rentDuration: rentDuration,
+      monthlyRent: monthlyRent,
+    });
+    getRentedHouses();
+  };
+  const deleteRentedHouse = async (ide) => {
+    await axios.delete(`http://localhost:5000/rented/${ide}`);
+    getRentedHouses();
+  };
+  const createRentedHouse = async () => {
+    await axios.post("http://localhost:5000/rented", {
+      customerId: customerId,
+      houseId: houseId,
+      rentStartDate: rentStartDate,
+      rentDuration: rentDuration,
+      monthlyRent: monthlyRent,
+    });
+    getRentedHouses();
+  };
+  const getRentedHouseById = async (ide) => {
+    const response = await axios.get(`http://localhost:5000/rented/${ide}`);
+    setCustomerId(response.data.customerId);
+    setRentStartDate(response.data.rentStartDate);
+    setHouseId(response.data.houseId);
+    setRentDuration(response.data.rentDuration);
+    setMonthlyRent(response.data.monthlyRent);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    switch (currentAction) {
+      case 0:
+        createRentedHouse();
+        break;
+      case 1:
+        updateRentedHouse();
+        break;
+    }
+    setIsFormActive(false);
+  };
+  const updateTableHandler = (ide) => {
+    setId(ide);
+    getRentedHouseById(ide);
+    setIsFormActive(true);
+  };
+  const deleteTableHandler = (ide) => {
+    setId(ide);
+    deleteRentedHouse(ide);
+  };
+
   const leftForm = () => {
     return (
       <Card.Body>
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setIsFormActive(false);
-          }}
-        >
+        <Form onSubmit={(e) => submitHandler(e)}>
           <Form.Group className="mb-2" controlId="FormBookId">
-            <Form.Label>Book Id</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Book Id"
-              //   value={bookId}
-              //   onChange={(event) => setBookId(event.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-2" controlId="FormBookName">
-            <Form.Label>Book Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Book Name"
-              //   value={bookName}
-              //   onChange={(event) => setBookName(event.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="mb-2" controlId="FormPublishedYear">
-            <Form.Label>Published Year</Form.Label>
+            <Form.Label>Customer Id</Form.Label>
             <Form.Control
               type="number"
-              placeholder="Published Year"
-              //   value={publishedYear}
-              //   onChange={(event) => setPublishedYear(event.target.value)}
+              placeholder="Enter Customer Id"
+              value={customerId}
+              onChange={(event) => setCustomerId(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-2" controlId="FormBookId">
+            <Form.Label>House Id</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter House Id"
+              value={houseId}
+              onChange={(event) => setHouseId(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-2" controlId="FormBookHouseId">
+            <Form.Label>Rent Start Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Enter Rent Start Date"
+              value={rentStartDate}
+              onChange={(event) => setRentStartDate(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-2" controlId="FormBookHouseId">
+            <Form.Label>Rent Duration</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter Rent Duration"
+              value={rentDuration}
+              onChange={(event) => setRentDuration(event.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-2" controlId="FormBookHouseId">
+            <Form.Label>Monthly Rent</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter Monthly Rent"
+              value={monthlyRent}
+              onChange={(event) => setMonthlyRent(event.target.value)}
             />
           </Form.Group>
           <Button type="submit" style={{ width: "100%", marginTop: "2vh" }}>
@@ -58,7 +151,7 @@ export default function RentedHouses() {
       addHandler={addHandler}
       updateHandler={updateHandler}
       deleteHandler={deleteHandler}
-      data={[{}]}
+      data={rentedHouses}
       headers={[
         "Customer Id",
         "House Id",
@@ -66,7 +159,13 @@ export default function RentedHouses() {
         "Rent Duration (in Months)",
         "Monthly Rent",
       ]}
-      keys={[]}
+      keys={[
+        "customerId",
+        "houseId",
+        "rentStartDate",
+        "rentDuration",
+        "monthlyRent",
+      ]}
       leftForm={leftForm}
     />
   );
