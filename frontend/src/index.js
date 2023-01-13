cfcbef7de312d7517b0eb52117542324ba57ea3b
AneeshMBhat <aneeshbhat3719@gmail.com>
2023-01-13 23:37:49 +0530

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+// reactJS is library so import it
 import ReactDOM from "react-dom/client";
+//document object model - layout of webpage
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+// we r using bootstrap for styling
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
